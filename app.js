79bcc1d3cb59b7ecc60d8a0e93b658555d2d1455
ralph william mujar <ralphwilliammujar@ralphs-MacBook-Air.local>
2021-02-24 23:54:30 +0800

    content changed

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello k8s!'))
+app.get('/', (req, res) => res.send('Hello kubernetes!'))
 app.listen(3000, () => console.log('Server ready'))
