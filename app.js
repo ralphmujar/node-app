447d4bbf0259571f0901c9fdfdd141da0d49a358
ralph william mujar <ralphwilliammujar@ralphs-MacBook-Air.local>
2021-02-26 13:59:33 +0800

    helm templates && content changed

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('new version deployed!'))
+app.get('/', (req, res) => res.send('ok goods'))
 app.listen(3000, () => console.log('Server ready'))
