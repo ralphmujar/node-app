17f9b1a7b0caed17676f203be044c0bebc1580e1
ralph william mujar <ralphwilliammujar@ralphs-MacBook-Air.local>
2021-02-26 15:31:41 +0800

    pipeline

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('ok goods'))
+app.get('/', (req, res) => res.send('ok sure'))
 app.listen(3000, () => console.log('Server ready'))
